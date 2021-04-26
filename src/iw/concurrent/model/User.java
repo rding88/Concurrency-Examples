86e4b2a118e5d1912fc8db6972324ae3f3916a5b
Richard Ding <rding88@yahoo.ca>
2021-04-26 11:31:25 -0400

    Added setter and getter for field email

--- src/iw/concurrent/model/User.java
+++ src/iw/concurrent/model/User.java
@@ -18,6 +18,14 @@ public class User {
 		this.name = name;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	@Override
 	public String toString() {
 		return "User [name=" + name + "]";
